b61202514d8b5b2f4e6487c38f7be3ffa28f9edd
Vanishaya <157686566+Vanishaya@users.noreply.github.com>
2026-05-20 02:04:02 +0700

    delete

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -10,7 +10,6 @@ export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
